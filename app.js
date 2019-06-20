23485bbf404a7ff2b95f3aec24292ba4f996dd6a
Guilherme Pejon <guilpejon@gmail.com>
2019-06-19 23:59:47 -0300

    Created logger

--- app.js
+++ app.js
@@ -6,17 +6,18 @@ const registriesRouter = require('./src/controllers/RegistryController')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const logger = require('./utils/logger')
 
 mongoose.set('useFindAndModify', false)
 
-console.log('connecting to', config.MONGODB_URI)
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connection to MongoDB:', error.message)
+    logger.error('error connection to MongoDB:', error.message)
   })
 
 app.use(cors())
