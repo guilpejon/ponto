9b1cefa8c4bc0ffccefeaab04a93e045f9a9dd09
Guilherme Pejon <guilpejon@gmail.com>
2019-06-22 20:50:14 -0300

    Improved tests

--- tests/registry_api.test.js
+++ tests/registry_api.test.js
@@ -6,121 +6,142 @@ const helper = require('./test_helper')
 const Registry = require('../src/models/Registry')
 const moment = require('moment')
 
-beforeEach(async () => {
-  await Registry.deleteMany({})
+describe('when there is initially some registries saved', () => {
+  beforeEach(async () => {
+    await Registry.deleteMany({})
 
-  let registryObject = new Registry(helper.initialRegistries[0])
-  await registryObject.save()
-})
+    // promises are executed in random order
+    const registryObjects = helper.initialRegistries
+      .map(registry => new Registry(registry))
+    const promiseArray = registryObjects.map(registry => registry.save())
+    await Promise.all(promiseArray) // wait for all the promises to end
 
-test('registries are returned as json', async () => {
-  await api
-    .get('/api/registries')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
-
-test('all registries are returned', async () => {
-  const response = await api.get('/api/registries')
-
-  expect(response.body.length).toBe(helper.initialRegistries.length)
-})
-
-test('a registry with createdAt can be added', async () => {
-  const now = moment()
-  const newRegistry = new Registry({
-    createdAt: now
+    // DOES NOT WORK, forEach does not wait for async calls to end
+    // helper.initialRegistries.forEach(async (registry) => {
+    //   let registryObject = new Registry(registry)
+    //   await registryObject.save()
+    // })
   })
 
-  await api
-    .post('/api/registries')
-    .send(newRegistry)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const registriesAtEnd = await helper.registriesInDb()
-  const response = await api.get('/api/registries')
-  expect(response.body.length).toBe(helper.initialRegistries.length + 1)
-
-  const registriesCreatedAt = registriesAtEnd.map(r => moment(r.createdAt).format())
-  expect(registriesCreatedAt).toContain(now.format())
-})
-
-test('an empty registry can be added', async () => {
-  const now = moment()
-  const newRegistry = new Registry({})
-
-  await api
-    .post('/api/registries')
-    .send(newRegistry)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const registriesAtEnd = await helper.registriesInDb()
-  const response = await api.get('/api/registries')
-  expect(response.body.length).toBe(helper.initialRegistries.length + 1)
-
-  // const registriesCreatedAt = registriesAtEnd.map(r => moment(r.createdAt).format())
-  // expect(registriesCreatedAt).toContain(now.format())
-})
-
-test('registry with createdAt in the past cannot be added', async () => {
-  const yesterday = moment().subtract(1, 'day')
-  const newRegistry = new Registry({
-    createdAt: yesterday
+  test('registries are returned as json', async () => {
+    await api
+      .get('/api/registries')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
   })
 
-  await api
-    .post('/api/registries')
-    .send(newRegistry)
-    .expect(400)
+  test('all registries are returned', async () => {
+    const response = await api.get('/api/registries')
 
-  const registriesAtEnd = await helper.registriesInDb()
-  const response = await api.get('/api/registries')
+    expect(response.body.length).toBe(helper.initialRegistries.length)
+  })
 
-  expect(response.body.length).toBe(helper.initialRegistries.length)
-})
+  describe('viewing a specific registry', () => {
+    test('succeeds with a valid id', async () => {
+      const notesAtStart = await helper.registriesInDb()
+      const registryToView = notesAtStart[0]
 
-test('a specific registry can be viewed', async () => {
-  const notesAtStart = await helper.registriesInDb()
-  const registryToView = notesAtStart[0]
+      const resultRegistry = await api
+        .get(`/api/registries/${registryToView.id}`)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 
-  const resultRegistry = await api
-    .get(`/api/registries/${registryToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+      expect(resultRegistry.body.id).toEqual(registryToView.id)
+    })
 
-  expect(resultRegistry.body.id).toEqual(registryToView.id)
-})
+    test('fails with 404 if registry does not exist', async () => {
+      const validNonExistingId = await helper.nonExistingId()
 
-test('a specific registry cannot be seen if it doesnt exist', async () => {
-  const resultRegistry = await api
-    .get('/api/registries/5d0eb9019e390f65e8050561')
-    .expect(404)
-})
+      const resultRegistry = await api
+        .get(`/api/registries/${validNonExistingId}`)
+        .expect(404)
+    })
 
-test('a specific registry cannot be seen if id is invalid', async () => {
-  const resultRegistry = await api
-    .get('/api/registries/wrong_id')
-    .expect(400)
-})
+    test('fail with 400 if id is invalid', async () => {
+      const invalidId = '5a3d5da59070081a82a3445'
 
-test('a registry can be deleted', async () => {
-  const notesAtStart = await helper.registriesInDb()
-  const registryToDelete = notesAtStart[0]
+      const resultRegistry = await api
+        .get(`/api/registries/${invalidId}`)
+        .expect(400)
+    })
+  })
 
-  await api
-    .delete(`/api/registries/${registryToDelete.id}`)
-    .expect(204)
+  describe('addition of a new registry', () => {
+    test('succeeds setting a valid createdAt value', async () => {
+      const now = moment()
+      const newRegistry = new Registry({
+        createdAt: now
+      })
 
-  const registriesAtEnd = await helper.registriesInDb()
+      await api
+        .post('/api/registries')
+        .send(newRegistry)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
 
-  expect(registriesAtEnd.length).toBe(
-    helper.initialRegistries.length - 1
-  )
+      const registriesAtEnd = await helper.registriesInDb()
+      const response = await api.get('/api/registries')
+      expect(response.body.length).toBe(helper.initialRegistries.length + 1)
 
-  // const contents = registriesAtEnd.map(r => r.content)
-  // expect(contents).not.toContain(registryToDelete.content)
+      const registriesCreatedAt = registriesAtEnd.map(r => moment(r.createdAt).format())
+      expect(registriesCreatedAt).toContain(now.format())
+    })
+
+    test('succeeds without setting a createdAt value', async () => {
+      const now = moment()
+      const newRegistry = new Registry({})
+
+      await api
+        .post('/api/registries')
+        .send(newRegistry)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+      const registriesAtEnd = await helper.registriesInDb()
+      const response = await api.get('/api/registries')
+      expect(response.body.length).toBe(helper.initialRegistries.length + 1)
+
+      // const registriesCreatedAt = registriesAtEnd.map(r => moment(r.createdAt).format())
+      // expect(registriesCreatedAt).toContain(now.format())
+    })
+
+    test('fails with 400 setting an invalid createdAt value', async () => {
+      const yesterday = moment().subtract(1, 'day')
+      const newRegistry = new Registry({
+        createdAt: yesterday
+      })
+
+      await api
+        .post('/api/registries')
+        .send(newRegistry)
+        .expect(400)
+
+      const registriesAtEnd = await helper.registriesInDb()
+      const response = await api.get('/api/registries')
+
+      expect(response.body.length).toBe(helper.initialRegistries.length)
+    })
+  })
+
+  describe('deletion of a registry', () => {
+    test('succeeds with 204 if id is valid', async () => {
+      const notesAtStart = await helper.registriesInDb()
+      const registryToDelete = notesAtStart[0]
+
+      await api
+        .delete(`/api/registries/${registryToDelete.id}`)
+        .expect(204)
+
+      const registriesAtEnd = await helper.registriesInDb()
+
+      expect(registriesAtEnd.length).toBe(
+        helper.initialRegistries.length - 1
+      )
+
+      // const contents = registriesAtEnd.map(r => r.content)
+      // expect(contents).not.toContain(registryToDelete.content)
+    })
+  })
 })
 
 afterAll(() => {
